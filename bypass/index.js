b792134a7a84e7d89ed80c5f7a7f373d34323d2a
y0d3n <yoden.923@gmail.com>
2024-05-15 09:14:04 +0900

    エスケープ漏れてた

--- bypass/index.js
+++ bypass/index.js
@@ -58,7 +58,7 @@ app.get("/shopping_quot", (req, res) => {
   let q = req.query.q ?? "";
   let query = "SELECT category,product,price FROM clothes ";
   if (q !== "") {
-    q = q.replace("'", "\\'");
+    q.replace(/'/g, "\\'");
     query += `WHERE category = '${q}' AND sale = 0;`;
   } else {
     query += "WHERE sale = 0;";
@@ -78,7 +78,7 @@ const length = "IDは1文字のみです";
 app.get("/users_length", (req, res) => {
   const id = req.query.id ?? "1";
   if (typeof id !== "string") {
-    return res.send("error")
+    return res.send("error");
   }
   if (id.length > 1) {
     return res.send(length);
@@ -90,7 +90,7 @@ app.get("/users_length", (req, res) => {
     return res.render("users.ejs", {
       users: results,
       query: query,
-      msg: length
+      msg: length,
     });
   });
 });
